6c1bc508eba9bc34fa9a54dec214b7a5129995a1
ugur <ugurkozgul@gmail.com>
2022-12-16 12:53:06 +0300

    testing işlemleri

---
+++ MvcWorkspace/Controllers/TestingController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MvcWorkspace.Controllers
+{
+    public class TestingController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewData["Message"] = "Controller' dan ..";
+            ////Dynamic Property
+            //ViewBag.Deneme = "Controllerdan BAG";
+
+            IEnumerable<SelectListItem> opsiyonlar = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text="Opsiyon 1",
+                    Value="1"
+                },
+                 new SelectListItem
+                {
+                    Text="Opsiyon 2",
+                    Value="2"
+                },
+                  new SelectListItem
+                {
+                    Text="Opsiyon 3",
+                    Value="3"
+                }
+            };
+
+            ViewBag.Dropdown = opsiyonlar;
+            ViewData["Opsiyonlar"] = opsiyonlar;
+            
+            
+            return View();
+
+        }
+    }
+}
