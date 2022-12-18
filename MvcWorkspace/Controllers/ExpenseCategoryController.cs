c7c13ca8e4b088fb657d5362f16e4f522322196a
Uğur Kozgül <ugurkozgul@gmail.com>
2022-12-18 23:18:24 +0300

    ExpenseCategoryController- View oluşturuldu-Layout link eklendi

---
+++ MvcWorkspace/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcWorkspace.Data;
+
+namespace MvcWorkspace.Controllers
+{
+    public class ExpenseCategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ExpenseCategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
