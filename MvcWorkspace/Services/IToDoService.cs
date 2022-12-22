a3ceefef779166cb4465654b70dd2115e5f52926
ugur <ugurkozgul@gmail.com>
2022-12-22 13:23:02 +0300

    dev-toDo'ya services eklendi GetTodoModels metodu hariç

---
+++ MvcWorkspace/Services/IToDoService.cs
@@ -0,0 +1,20 @@
+﻿using MvcWorkspace.Models;
+
+namespace MvcWorkspace.Services
+{
+    public interface IToDoService
+    {
+        IEnumerable<ToDoModel> GetToDoModels();
+        
+        IEnumerable<ToDoModel> GetAll();
+
+        bool Add (ToDoModel toDoModel);
+
+        bool Update (ToDoModel toDoModel);
+
+        bool Delete (int id);
+
+        bool Finisher(int id);
+
+    }
+}
---
+++ MvcWorkspace/Services/ToDoService.cs
@@ -0,0 +1,83 @@
+﻿using MvcWorkspace.Data;
+using MvcWorkspace.Models;
+
+namespace MvcWorkspace.Services
+{
+    public class ToDoService : IToDoService
+    {
+        private readonly AppDbContext _db;
+
+        public ToDoService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Add(ToDoModel toDoModel)
+        {
+            try
+            {
+                _db.Add(toDoModel);
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var toDoModel = _db.ToDoModels.Find(id);
+                _db.ToDoModels.Remove(toDoModel);
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Finisher(int id)
+        {
+            var toDoModel = _db.ToDoModels.Find(id);
+            toDoModel.IsItOver = true;
+            _db.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<ToDoModel> GetAll()
+        {
+            return _db.ToDoModels;
+
+        }
+
+        public IEnumerable<ToDoModel> GetToDoModels()
+        {
+
+            //IEnumerable<ToDoModel> toDoModels = _db.ToDoModels.Where()
+            throw new NotImplementedException();
+        }
+
+        public bool Update(ToDoModel toDoModel)
+        {
+            try
+            {
+                _db.Update(toDoModel);
+                _db.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+
+            }
+        }
+    }
+}
