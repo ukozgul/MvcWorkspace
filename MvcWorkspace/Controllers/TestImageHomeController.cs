994a88bdaeed6f53a5313d79b936538c2c4f876c
ugur <ugurkozgul@gmail.com>
2022-12-23 14:12:16 +0300

    urlmodel oluşturuldu

---
+++ MvcWorkspace/Controllers/TestImageHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWorkspace.Controllers
+{
+    public class TestImageHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcWorkspace/Models/TestM.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcWorkspace.Models
+{
+    public class TestM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
